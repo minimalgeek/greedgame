56bae19d46b55fd61dfb37a2ad45fb3f1925d8ee
minimalgeek <minimalgeek@gmail.com>
2020-05-28 13:00:42 +0200

    improve on ScoreEvaluator with immutable stream

--- src/main/java/com/greed/game/implementation/ScoreEvaluator.java
+++ src/main/java/com/greed/game/implementation/ScoreEvaluator.java
@@ -3,32 +3,32 @@ package com.greed.game.implementation;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class ScoreEvaluator {
     public int evaluate(int[] arrayOfDiceRollResult) {
-
-        Map<Integer, Integer> counter = new HashMap<>();
-        Arrays.stream(arrayOfDiceRollResult)
-                .forEach(value -> counter.merge(value, 1, Integer::sum));
+        Map<Integer, Long> counter = Arrays.stream(arrayOfDiceRollResult).boxed().collect(
+                Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
 
         if (counter.entrySet().stream().allMatch(integerIntegerEntry -> integerIntegerEntry.getValue() == 2))
             return 800;
         if (counter.entrySet().size() == 6 &&
-            counter.entrySet().stream().allMatch(integerIntegerEntry -> integerIntegerEntry.getValue() == 1))
+                counter.entrySet().stream().allMatch(integerIntegerEntry -> integerIntegerEntry.getValue() == 1))
             return 1200;
 
         return counter.entrySet().stream()
                 .mapToInt(this::countingSortBasedScoreCalculation).sum();
     }
 
-    private int countingSortBasedScoreCalculation(Map.Entry<Integer, Integer> keyValuePair) {
+    private int countingSortBasedScoreCalculation(Map.Entry<Integer, Long> keyValuePair) {
         Integer key = keyValuePair.getKey();
-        Integer amount = keyValuePair.getValue();
+        Long amount = keyValuePair.getValue();
 
-        int multiplier = amount >= 3 ? 100 * (int) Math.pow(2, amount-3) : 1;
+        int multiplier = amount >= 3 ? 100 * (int) Math.pow(2, amount - 3) : 1;
 
         if (amount >= 3) {
             if (key == 1) {
@@ -38,7 +38,7 @@ public class ScoreEvaluator {
         } else {
             if (key == 1) {
                 return 100;
-            } else if(key == 5) {
+            } else if (key == 5) {
                 return 50;
             }
         }
